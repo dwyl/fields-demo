6a602344c86e7217ef4d852090dc9e3691c8e45e
nelsonic <nelson+github@dwyl.com>
2023-09-18 03:04:54 +0100

    Step 2.1 create coverallls.json to ignore untested files

--- lib/fields_demo_web/router.ex
+++ lib/fields_demo_web/router.ex
@@ -10,18 +10,9 @@ defmodule FieldsDemoWeb.Router do
     plug :put_secure_browser_headers
   end
 
-  pipeline :api do
-    plug :accepts, ["json"]
-  end
-
   scope "/", FieldsDemoWeb do
     pipe_through :browser
 
     get "/", PageController, :home
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", FieldsDemoWeb do
-  #   pipe_through :api
-  # end
 end
